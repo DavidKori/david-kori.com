44ea7889b7fb73867cae3cc6ad8ec489397d46e7
KoriDavid <mutugidavid37@gmail.com>
2026-01-06 08:21:07 +1100

    add remote api routes

--- src/services/axios.js
+++ src/services/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   // baseURL:'http://localhost:5000/api',
-  baseURL: 'https://portfolio-backend-xvu9.onrender.com',
+  baseURL: 'https://portfolio-backend-xvu9.onrender.com/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
