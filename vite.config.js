0a145e2ddb95eb4258cda1e64228e1e4d1afd49c
KoriDavid <mutugidavid37@gmail.com>
2026-03-11 23:39:35 +0300

    server call issue due to domain changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/www.korideveloper.work.gd/"
+  base:"/david-kori.com/"
 })
